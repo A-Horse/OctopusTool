dadfb3dd3b4d398411112d3ec29c5700214f684d
fwchen <crystal.tyan@gmail.com>
2017-09-08 14:40:16 +0800

    change title

--- pages/index.vue
+++ pages/index.vue
@@ -8,21 +8,21 @@
 </template>
 
 <script>
-export default {
-  head () {
-    return {
-      title: 'Developer and Designer Tool - Octopus Tool',
-      meta: [
-        { hid: 'description', name: 'description', content: 'Generate low poly image with a click!' },
-        { hid: 'og:title', name: 'og:title', content: 'Design Tool - Low poly image generation.' },
-        { hid: 'og:type', name: 'og:type', content: 'website' }
-      ]
-    }
-  },
-  components: {
+  export default {
+    head () {
+      return {
+        title: 'Tool Octopuese - Online | Developer and Designer Tool - Octopus Tool',
+        meta: [
+          { hid: 'description', name: 'description', content: 'Generate low poly image with a click!' },
+          { hid: 'og:title', name: 'og:title', content: 'Design Tool - Low poly image generation.' },
+          { hid: 'og:type', name: 'og:type', content: 'website' }
+        ]
+      }
+    },
+    components: {
 
+    }
   }
-}
 </script>
 
 <style scoped>
--- pages/low-poly/index.vue
+++ pages/low-poly/index.vue
@@ -51,106 +51,106 @@
 </template>
 
 <script>
-import LowPolyWorker from '../../worker/low-poly.worker.js'
+  import LowPolyWorker from '../../worker/low-poly.worker.js'
 
-export default {
-  head () {
-    return {
-      title: 'Generate low poly image',
-      meta: [
-        { hid: 'description', name: 'description', content: 'Generate low poly image with a click!' },
-        { hid: 'og:title', name: 'og:title', content: 'Design Tool - Low poly image generation.' }
-      ]
-    }
-  },
-  components: {
-  },
-  data () {
-    return {
-      jxd: 50,
-      random: 100,
-      img: null,
-      running: false,
-      worker: null,
-      fileName: 'low-poly'
-    }
-  },
-  methods: {
-    download () {
-      const link = this.$el.querySelector('.download-link')
-      link.href = this.$el.querySelector('canvas').toDataURL()
-      link.download = 'lp_' + this.fileName
-    },
-    onChange (event) {
-      const input = event.target
-      if (!input.files.length) return
-      var file = input.files[0]
-      this.fileName = file.name
-      if (!/\/(?:jpeg|png)/i.test(file.type)) {
-        alert('图片需为JPG或者PNG格式')
-        return
+  export default {
+    head () {
+      return {
+        title: 'Tool Octopuese - Online Low-Poly Generater | low poly image',
+        meta: [
+          { hid: 'description', name: 'description', content: 'Generate low poly image with a click!' },
+          { hid: 'og:title', name: 'og:title', content: 'Design Tool - Low poly image generation.' }
+        ]
       }
-
-      const self = this
-      var reader = new FileReader()
-      reader.onload = function () {
-        var img = new Image()
-        img.src = this.result
-        img.onload = function () {
-          self.img = this
-          setTimeout(() => {
-            self.parse()
-          })
+    },
+    components: {
+    },
+    data () {
+      return {
+        jxd: 50,
+        random: 100,
+        img: null,
+        running: false,
+        worker: null,
+        fileName: 'low-poly'
+      }
+    },
+    methods: {
+      download () {
+        const link = this.$el.querySelector('.download-link')
+        link.href = this.$el.querySelector('canvas').toDataURL()
+        link.download = 'lp_' + this.fileName
+      },
+      onChange (event) {
+        const input = event.target
+        if (!input.files.length) return
+        var file = input.files[0]
+        this.fileName = file.name
+        if (!/\/(?:jpeg|png)/i.test(file.type)) {
+          alert('图片需为JPG或者PNG格式')
+          return
         }
-      }
-      reader.readAsDataURL(file)
-    },
-    parse () {
-      if (!this.img) {
-        return
-      }
-      if (this.running) {
-        this.worker.terminate()
-      }
-      this.running = true
-      const img = this.img
-      const jxdvalue = this.jxd
-      const canvas = this.$el.querySelector('canvas')
-      const ctx = canvas.getContext('2d')
 
-      canvas.width = img.width
-      canvas.height = img.height * canvas.width / img.width
-
-      ctx.drawImage(img, 0, 0, canvas.width, canvas.height)
-      this.worker = new LowPolyWorker()
-      setTimeout(() => {
-        var imgData = ctx.getImageData(0, 0, canvas.width, canvas.height)
-        this.worker.postMessage({
-          imgData: imgData,
-          width: canvas.width,
-          height: canvas.height,
-          jxdvalue
-        })
-        this.worker.addEventListener('message', ({ data }) => {
-          if (data.finish) {
-            this.running = false
-            return
+        const self = this
+        var reader = new FileReader()
+        reader.onload = function () {
+          var img = new Image()
+          img.src = this.result
+          img.onload = function () {
+            self.img = this
+            setTimeout(() => {
+              self.parse()
+            })
           }
-          const { x1, x2, x3, y1, y2, y3, colorr, colorg, colorb } = data
-          ctx.save()
-          ctx.beginPath()
-          ctx.moveTo(x1, y1)
-          ctx.lineTo(x2, y2)
-          ctx.lineTo(x3, y3)
-          ctx.closePath()
-          ctx.fillStyle = 'rgba(' + colorr + ',' + colorg + ',' + colorb + ',1)'
-          ctx.fill()
-          ctx.restore()
-        })
-      }, 50)
+        }
+        reader.readAsDataURL(file)
+      },
+      parse () {
+        if (!this.img) {
+          return
+        }
+        if (this.running) {
+          this.worker.terminate()
+        }
+        this.running = true
+        const img = this.img
+        const jxdvalue = this.jxd
+        const canvas = this.$el.querySelector('canvas')
+        const ctx = canvas.getContext('2d')
+
+        canvas.width = img.width
+        canvas.height = img.height * canvas.width / img.width
+
+        ctx.drawImage(img, 0, 0, canvas.width, canvas.height)
+        this.worker = new LowPolyWorker()
+        setTimeout(() => {
+          var imgData = ctx.getImageData(0, 0, canvas.width, canvas.height)
+          this.worker.postMessage({
+            imgData: imgData,
+            width: canvas.width,
+            height: canvas.height,
+            jxdvalue
+          })
+          this.worker.addEventListener('message', ({ data }) => {
+            if (data.finish) {
+              this.running = false
+              return
+            }
+            const { x1, x2, x3, y1, y2, y3, colorr, colorg, colorb } = data
+            ctx.save()
+            ctx.beginPath()
+            ctx.moveTo(x1, y1)
+            ctx.lineTo(x2, y2)
+            ctx.lineTo(x3, y3)
+            ctx.closePath()
+            ctx.fillStyle = 'rgba(' + colorr + ',' + colorg + ',' + colorb + ',1)'
+            ctx.fill()
+            ctx.restore()
+          })
+        }, 50)
+      }
     }
   }
-}
 </script>
 
 <style scoped>
